a83edbd7443118d593028acb13815b3c76a0bb94
NadyaNIS <nadyaimanthie@gmail.com>
2017-08-04 21:25:37 +0530

    Angelo's journey and journeyListClass

---
+++ src/controllers/JourneyList.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import models.Journey;
+
+/**
+ *
+ * @author Siri@MnS
+ */
+public class JourneyList extends ArrayList<Journey>{
+   
+    private Journey Journey;
+    ArrayList<Journey> journeyArr = new ArrayList<Journey>();
+    ArrayList<Journey> journey2 = new ArrayList<Journey>();
+    public static final File file = new File("Journey.txt");
+    private JourneyList journey ;
+    private int count;
+    
+    public JourneyList()  
+{
+    journeyArr = Deserialize(file);
+    count  = journeyArr.size();
+    Journey.setJourneyCount(count);
+    
+    //super();
+}
+    public void addJourney(Journey journey)
+{
+    //super.add(aBook);
+    this.Journey = journey; 
+    journeyArr = Deserialize(file);
+    journeyArr.add(Journey);
+    Serialize(journeyArr);
+      
+}
+    public ArrayList findJourneysByDate (Date jdate)
+ {
+    //books2 = null;
+    journeyArr = Deserialize(file);
+    for (Journey i : journeyArr){
+        if (i.getJourneyDate().equals(jdate)){
+            journey2.add(i);
+        }
+    }
+    return journey2;
+}
+    public int getTotalJourneys(){
+        journeyArr = Deserialize(file);
+        count  = journeyArr.size();
+        return count;
+    }
+    
+    public void Serialize( ArrayList arraylist){
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            
+            oos.writeObject(arraylist);
+            fos.close();
+            System.out.println("Serialized");
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+  public ArrayList Deserialize(File filename){
+        
+        ArrayList d_arraylist = null;
+        try{
+            FileInputStream fis = new FileInputStream(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            
+            d_arraylist = (ArrayList)ois.readObject();
+            fis.close();
+            System.out.println("Deserialized");
+            System.out.println(d_arraylist);
+            
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } 
+        
+        return d_arraylist;
+    }
+}
--- src/models/Journey.java
+++ src/models/Journey.java
@@ -11,26 +11,116 @@ import java.util.Date;
 public class Journey implements Serializable  {
     
     private token tokenObj;
+    private String tokenID;
     private float charge;
     private NetworkStop startStop;
     private Date startTime;
     private NetworkStop stopStop;
     private Date stopTime;
     private Network networkType;
-    //private Route route;
-      
-    public Journey( Float charge,NetworkStop startStop, Date startTime, NetworkStop stopstop, Date stoptime){
+    private Route route;
     
-        //this.tokenID = tokenID;
+    private Date journeyDate;
+    private static int journeyCount=0;
+      
+    public Journey( String tokenID, float charge, NetworkStop startStop, Date startTime, NetworkStop stopstop, Date stoptime, Network networkType, Route route){
+    
+        this.tokenID = tokenID;
         this.charge = charge;
         this.startStop = startStop;
         this.startTime = startTime;
         this.stopStop = stopStop;
         this.stopTime = stopTime;
-        //this.networkType = networkType;
-        //this.route = route;
+        this.networkType = networkType;
+        this.route = route;
            
     }
+    
+    public void endJourney(NetworkStop networkStop, float charge){
+    //reduce the amount from account balance.. check if this is to be implemented in another class
+    }
+
+    public static int getJourneyCount() {
+        return journeyCount;
+    }
+
+    public static void setJourneyCount(int journeyCount) {
+        Journey.journeyCount = journeyCount;
+    }
+
+    public Date getJourneyDate() {
+        return journeyDate;
+    }
+
+    public void setJourneyDate(Date journeyDate) {
+        this.journeyDate = journeyDate;
+    }
+    
+    public String getTokenID() {
+        return tokenID;
+    }
+
+    public void setTokenID(String tokenID) {
+        this.tokenID = tokenID;
+    }
+
+    public float getCharge() {
+        return charge;
+    }
+
+    public void setCharge(float charge) {
+        this.charge = charge;
+    }
+
+    public NetworkStop getStartStop() {
+        return startStop;
+    }
+
+    public void setStartStop(NetworkStop startStop) {
+        this.startStop = startStop;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public NetworkStop getStopStop() {
+        return stopStop;
+    }
+
+    public void setStopStop(NetworkStop stopStop) {
+        this.stopStop = stopStop;
+    }
+
+    public Date getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(Date stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public Network getNetworkType() {
+        return networkType;
+    }
+
+    public void setNetworkType(Network networkType) {
+        this.networkType = networkType;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+    
+    
 
     
 }
