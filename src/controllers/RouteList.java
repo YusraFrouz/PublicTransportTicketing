214a6ce73241142ab2e591d39ee8ad3bc148cc90
NadyaNIS <nadyaimanthie@gmail.com>
2017-08-07 07:05:46 +0530

    pra's

--- src/controllers/RouteList.java
+++ src/controllers/RouteList.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import static controllers.NetworkList.file;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,6 +11,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import models.Route;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,6 +26,7 @@ import models.Route;
 public class RouteList extends ArrayList<Route>{
     private Route route;
     ArrayList<Route> routeArr = new ArrayList<Route>();
+    ArrayList<Integer> routeArr1 = new ArrayList<Integer>();
     public static final File file = new File("Routes.txt");
     
     public RouteList(){
@@ -35,19 +38,25 @@ public class RouteList extends ArrayList<Route>{
         this.route = pRoute; 
         routeArr = Deserialize(file);
         int id = route.getRouteID();
+        System.out.println(id);
+        
+        
         for(Route i : routeArr){
-            if(i.getRouteID() == id)
-            {
-                DisplayMessage.display("Network already exists!");
+           if(i.getRouteID() == id)
+           {
+                DisplayMessage.display("Route already exists!");
                 System.out.println("exists");
-                break;  
-            }
-            else
-            {
+                break;
+           } 
+           else
+           {
                 routeArr.add(route);
+                DisplayMessage.display("Route added successfully!");
                 System.out.println("added");
-            }    
+                break;
+           }    
         }
+        
         Serialize(routeArr);
       
     }
@@ -57,6 +66,17 @@ public class RouteList extends ArrayList<Route>{
         routeArr = Deserialize(file);
         return routeArr;
     }
+    
+    public ArrayList getRouteID (){
+    routeArr = Deserialize(file);
+    for (Route i : routeArr){
+        routeArr1.add(i.getRouteID());
+        System.out.println(i.getRouteID());
+        
+    }
+    return routeArr1;
+        
+}
     
     public void Serialize( ArrayList arraylist){
         try{
